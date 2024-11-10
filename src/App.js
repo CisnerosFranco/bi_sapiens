e3294cbbc7aaa351dbd4376dbc90ea237e29362e
Franco Cisneros <francocisneros147@gmail.com>
2024-11-10 19:46:21 -0300

    first commit yeah

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from "./imagenes/logo1.png"
+import img1 from "./imagenes/img_1.jpg"
+import img2 from './imagenes/img_2.png'
+import img3 from './imagenes/img_3.jpg'
+import img4 from './imagenes/img_4.webp'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='containerLogo'>
+          <img src={logo} className='Logo-header'/>
+        </div>
+        <p className='header-titulo'>BI SAPIENS</p> 
       </header>
+      <main>
+        <div className='presentacion'>
+          <p className='text'><span className='negrita'>Contratar nuestros servicios de BI</span> es la mejor inversión para tu empresa, 
+          ya que permite transformar datos en información útil y tomar decisiones basadas en hechos.</p>
+          <p className='text'>
+           <span className='negrita'>Business Intelligence (BI)</span>, es el proceso de recopilar, analizar y transformar datos en información útil, que resultara de mucho beneficio para la toma de decisiones estratégicas. 
+          El BI hace posible el rapido acceso y la fácil lectura de los datos más relevantes, identificar patrones y tendencias como los tiempos de trabajo, gastos, rendimientos y resultados. 
+          De esta forma podras conocer mejor el funcionamiento de tu negocio.
+          </p>
+        </div>
+        <div className='art-1 art'>
+          <img src={img2} className='art-1-img'/>
+        </div>
+        <div className='servicios'>
+          <p className='segundo-titulo'>NUESTROS SERVICIOS</p>
+          <div className=''>
+
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
+
+const style = StyleSheet.Cre
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+
+export default Header = ({ urlLogo = "" }) => {
+
+    return (
+        <>
+            <div className="Header">
+                <img src="../imagenes/tshirt.png"/>
+                <h1>BI SAPIENS</h1> 
+            </div>
+        </>
+    )
+
+}
+
+
+
+
+
+
